cdc78f0c959d4c9ab1237c8a63fee0b5bb9155ca
Moose17 <63780114+Mooose17@users.noreply.github.com>
2023-03-09 19:50:00 +0000

    adds compents to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-import Homepage from "@/components/Home";
+import Navbar from "@/components/Navbar";
+import HomeContent from "@/components/HomeContent";
 
 export default function Home() {
   return (
     <div>
-      <Homepage />
+      <Navbar />
+      <HomeContent />
     </div>
   );
 }
