0bd535f4c8cad96e5155911fb1ee9158ddfef48b
Moose17 <63780114+Mooose17@users.noreply.github.com>
2023-06-25 22:12:33 +0100

    update footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,9 +3,24 @@ import Styles from "./styling/Footer.module.css";
 
 const Footer = () => {
   return (
-    <div className={Styles.footerContainer}>
-      <p className={Styles.footerText}>Moose 2023©</p>
-    </div>
+    <>
+      <div className={Styles.footerContainer}>
+        <p className={Styles.footerText}>Moose 2023©</p>
+        <div className={Styles.socialsContainer}>
+          <ul className={Styles.socials}>
+            <a href="https://www.linkedin.com/in/tommybowden/">
+              <li>LinkedIn 🔗</li>
+            </a>
+            <a href="https://twitter.com/Tommoose_">
+              <li>Twitter 🔗</li>
+            </a>
+            <a href="https://chillpeep.zone/@Moose">
+              <li>Mastodon 🔗</li>
+            </a>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 };
 
